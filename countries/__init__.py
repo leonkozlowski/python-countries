c3db52607cc774d7c94e25a136f30187da81e8da
leonkozlowski <leonkozlowski@gmail.com>
2019-12-17 22:50:30 -0500

    added other endpoints

--- countries/__init__.py
+++ countries/__init__.py
@@ -1,5 +1,6 @@
 """python-countries"""
 
+import enum
 import logging
 import re
 import requests
@@ -26,9 +27,62 @@ class CountriesApi(object):
 
         >>> client = CountriesApi()
 
-        >>> client.full_name(name='United States')
+        >>> client.full_name(name='Colombia')
 
-        >>>
+    Returns:
+        [{
+            "name": "Colombia",
+            "topLevelDomain": [".co"],
+            "alpha2Code": "CO",
+            "alpha3Code": "COL",
+            "callingCodes": ["57"],
+            "capital": "Bogotá",
+            "altSpellings": ["CO", "Republic of Colombia", "República de Colombia"],
+            "region": "Americas",
+            "subregion": "South America",
+            "population": 48759958,
+            "latlng": [4.0, -72.0],
+            "demonym": "Colombian",
+            "area": 1141748.0,
+            "gini": 55.9,
+            "timezones": ["UTC-05:00"],
+            "borders": ["BRA", "ECU", "PAN", "PER", "VEN"],
+            "nativeName": "Colombia",
+            "numericCode": "170",
+            "currencies": [{
+                "code": "COP",
+                "name": "Colombian peso",
+                "symbol": "$"
+            }],
+            "languages": [{
+                "iso639_1": "es",
+                "iso639_2": "spa",
+                "name": "Spanish",
+                "nativeName": "Español"
+            }],
+            "translations": {
+                "de": "Kolumbien",
+                "es": "Colombia",
+                "fr": "Colombie",
+                "ja": "コロンビア",
+                "it": "Colombia",
+                "br": "Colômbia",
+                "pt": "Colômbia"
+            },
+            "flag": "https://restcountries.eu/data/col.svg",
+            "regionalBlocs": [{
+                "acronym": "PA",
+                "name": "Pacific Alliance",
+                "otherAcronyms": [],
+                "otherNames": ["Alianza del Pacífico"]
+            }, {
+                "acronym": "USAN",
+                "name": "Union of South American Nations",
+                "otherAcronyms": ["UNASUR", "UNASUL", "UZAN"],
+                "otherNames": ["Unión de Naciones Suramericanas"..."]
+            }],
+            "cioc": "COL"
+        }]
     """
 
     def __init__(self):
@@ -77,7 +131,7 @@ class CountriesApi(object):
             resp: (dict) - json response object
         """
         if not name:
-            raise ValueError("Please enter a name for country_name request")
+            raise ValueError("Please enter a name for python-countries request")
 
         stripped_name = re.sub(r"\s+", "", name, flags=re.UNICODE)
         resp_obj = requests.get(url=f"{self.base_url}name/{stripped_name.lower()}")
@@ -101,7 +155,7 @@ class CountriesApi(object):
             resp: (dict) - json response object
         """
         if not name:
-            raise ValueError("Please enter a name for country_name request")
+            raise ValueError("Please enter a name for python-countries request")
 
         stripped_name = re.sub(r"\s+", "", name, flags=re.UNICODE)
         url = f"{self.base_url}name/{stripped_name.lower()}"
@@ -129,7 +183,7 @@ class CountriesApi(object):
             resp: (dict) - json response object
         """
         if not code:
-            raise ValueError("Please enter a name for country_name request")
+            raise ValueError("Please enter a iso_code for python-countries request")
 
         elif 2 < len(code) < 3:
             raise ValueError("ISO 3166-1 code must be 2 or 3 characters")
@@ -157,7 +211,7 @@ class CountriesApi(object):
             resp: (dict) - json response object
         """
         if not currency:
-            raise ValueError("Please enter a name for country_name request")
+            raise ValueError("Please enter a currency for python-countries request")
 
         url = f"{self.base_url}currency/{currency.lower()}"
 
@@ -182,9 +236,99 @@ class CountriesApi(object):
             resp: (dict) - json response object
         """
         if not language:
-            raise ValueError("Please enter a name for country_name request")
+            raise ValueError("Please enter a language for python-countries request")
 
         url = f"{self.base_url}lang/{language.lower()}"
+
+        resp_obj = requests.get(url=url)
+        self._check_http_status(resp_obj.status_code)
+
+        resp = resp_obj.json()
+
+        return resp
+
+    def capital_city(self, city: str) -> dict:
+        """
+        Request capital city endpoint
+        Search by capital city
+
+            (i.e) - "https://restcountries.eu/rest/v2/capital/tallinn"
+
+        Args:
+            city: (str) - capital city
+
+        Returns:
+            resp: (dict) - json response object
+        """
+        if not city:
+            raise ValueError("Please enter a capital city for python-countries request")
+
+        stripped_city = re.sub(r"\s+", "", city, flags=re.UNICODE)
+        url = f"{self.base_url}capital/{stripped_city.lower()}"
+
+        resp_obj = requests.get(url=url)
+        self._check_http_status(resp_obj.status_code)
+
+        resp = resp_obj.json()
+
+        return resp
+
+    def calling_code(self, calling_code: int) -> dict:
+        """
+        Request capital city endpoint
+        Search by calling code
+
+            (i.e) - "https://restcountries.eu/rest/v2/callingcode/372"
+
+        Args:
+            calling_code: (str) - calling code
+            (ref: https://countrycode.org/)
+
+        Returns:
+            resp: (dict) - json response object
+        """
+        if not calling_code:
+            raise ValueError("Please enter a calling code for python-countries request")
+
+        url = f"{self.base_url}callingcode/{str(calling_code)}"
+
+        resp_obj = requests.get(url=url)
+        self._check_http_status(resp_obj.status_code)
+
+        resp = resp_obj.json()
+
+        return resp
+
+    def region(self, region: str) -> dict:
+        """
+        Request capital city endpoint
+        Search by calling code
+
+            (i.e) - "https://restcountries.eu/rest/v2/callingcode/372"
+
+        Args:
+            region: (enum.Enum) - region
+
+        Returns:
+            resp: (dict) - json response object
+        """
+
+        class ExtendedEnum(enum.Enum):
+            @classmethod
+            def list(cls):
+                return list(map(lambda c: c.value, cls))
+
+        class Region(ExtendedEnum):
+            AFRICA = 'africa'
+            AMERICAS = 'americas'
+            ASIA = 'asia'
+            EUROPE = 'europe'
+            OCEANIA = 'oceania'
+
+        if not region in Region._value2member_map_:
+            raise ValueError(f"Valid regions are: {Region.list()}")
+
+        url = f"{self.base_url}region/{region.lower()}"
 
         resp_obj = requests.get(url=url)
         self._check_http_status(resp_obj.status_code)
